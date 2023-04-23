2a3f9daebcaea51deb6ee57a977b4b9568ced3cc
gorbatschow <gorbatschow.r@gmail.com>
2023-04-23 08:23:52 +0300

    update ImOsm submodule

--- src/ImOsmDemoApp.cpp
+++ src/ImOsmDemoApp.cpp
@@ -18,38 +18,48 @@ void ImOsmDemoApp::paint() {
   ImGui::Begin("MapWidget");
 
   ImGui::Text("FPS: %.0f", ImGui::GetIO().Framerate);
-  ImGui::Text("MOUSE: lon %.2f, lat %.2f", _mapPlot.mouseLon(),
-              _mapPlot.mouseLat());
-  ImGui::Text("VIEW: lon %.2f-%.2f, lat %.2f-%.2f ", _mapPlot.minLon(),
-              _mapPlot.maxLon(), _mapPlot.minLat(), _mapPlot.maxLat());
+  ImGui::Text("MOUSE: lat %.2f, lon %.2f",
+              _mapPlot->mouseLon(),
+              _mapPlot->mouseLat());
+  ImGui::Text("VIEW: lat %.2f:%.2f, lon %.2f:%.2f ",
+              _mapPlot->minLat(),
+              _mapPlot->maxLat(),
+              _mapPlot->minLon(),
+              _mapPlot->maxLon());
 
   // ImGui::Text("Tiles: %d", _mapPlot.loader().tilesNum());
 
   _worldBtn.paint();
   if (_worldBtn.handle()) {
-    _mapPlot.setBoundsGeo(ImOsm::MinLon, ImOsm::MaxLon, ImOsm::MinLat,
-                          ImOsm::MaxLat);
+    _mapPlot->setBoundsGeo(ImOsm::MinLat,
+                           ImOsm::MaxLat,
+                           ImOsm::MinLon,
+                           ImOsm::MaxLon);
   }
 
   ImGui::SameLine();
   _berlinBtn.paint();
   if (_berlinBtn.handle()) {
-    _mapPlot.setBoundsGeo(13.12, 13.66, 52.43, 52.59);
+    _mapPlot->setBoundsGeo(52.43, 52.59, 13.12, 13.66);
   }
 
   ImGui::SameLine();
   _parisBtn.paint();
   if (_parisBtn.handle()) {
-    _mapPlot.setBoundsGeo(1.12, 3.58, 49.25, 48.57);
+    _mapPlot->setBoundsGeo(49.25, 48.57, 1.12, 3.58);
   }
 
   ImGui::SameLine();
   _madridBtn.paint();
   if (_madridBtn.handle()) {
-    _mapPlot.setBoundsGeo(-4.26, -3.10, 40.61, 40.19);
+    _mapPlot->setBoundsGeo(40.61, 40.19, -4.26, -3.10);
   }
 
-  _mapPlot.paint();
+  _mapPlot->paint();
 
+  ImGui::End();
+
+  ImGui::Begin("MarkWidget");
+  _markWidget->paint();
   ImGui::End();
 }
--- src/ImOsmDemoApp.h
+++ src/ImOsmDemoApp.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <imapp.h>
 #include <imosm.h>
+#include <imosm_rich.h>
 #include <imwrap.h>
 
 class ImOsmDemoApp : public ImApp::MainWindow {
@@ -19,5 +20,10 @@ private:
   ImWrap::Button _madridBtn{"Madrid"};
   bool _firstPaint{true};
 
-  ImOsm::MapPlot _mapPlot;
+  std::shared_ptr<ImOsm::RichMapPlot> _mapPlot{
+      std::make_shared<ImOsm::RichMapPlot>()};
+  std::shared_ptr<ImOsm::RichMarkStorage> _markStorage{
+      std::make_shared<ImOsm::RichMarkStorage>()};
+  std::unique_ptr<ImOsm::RichMarkWidget> _markWidget{
+      std::make_unique<ImOsm::RichMarkWidget>(_mapPlot, _markStorage)};
 };
