73378a53e1a190bc2bb1651ce10bf00e4d296bbd
gorbatschow <gorbatschow.r@gmail.com>
2023-04-23 16:54:27 +0300

    update submodule imosm

--- src/ImOsmDemoApp.h
+++ src/ImOsmDemoApp.h
@@ -20,20 +20,20 @@ private:
   ImWrap::Button _parisBtn{"Paris"};
   ImWrap::Button _madridBtn{"Madrid"};
 
-  std::shared_ptr<ImOsm::RichMapPlot> _mapPlot{
-      std::make_shared<ImOsm::RichMapPlot>()};
+  std::shared_ptr<ImOsm::Rich::RichMapPlot> _mapPlot{
+      std::make_shared<ImOsm::Rich::RichMapPlot>()};
 
-  std::shared_ptr<ImOsm::RichMarkStorage> _markStorage{
-      std::make_shared<ImOsm::RichMarkStorage>()};
+  std::shared_ptr<ImOsm::Rich::MarkStorage> _markStorage{
+      std::make_shared<ImOsm::Rich::MarkStorage>()};
 
-  std::unique_ptr<ImOsm::RichMarkEditorWidget> _markEditorWidget{
-      std::make_unique<ImOsm::RichMarkEditorWidget>(_mapPlot, _markStorage)};
+  std::unique_ptr<ImOsm::Rich::MarkEditorWidget> _markEditorWidget{
+      std::make_unique<ImOsm::Rich::MarkEditorWidget>(_mapPlot, _markStorage)};
 
-  std::unique_ptr<ImOsm::RichDistanceCalcWidget> _distanceCalcWidget{
-      std::make_unique<ImOsm::RichDistanceCalcWidget>(_markStorage)};
+  std::unique_ptr<ImOsm::Rich::DistanceCalcWidget> _distanceCalcWidget{
+      std::make_unique<ImOsm::Rich::DistanceCalcWidget>(_markStorage)};
 
-  std::unique_ptr<ImOsm::RichDestinationCalcWidget> _destinationCalcWidget{
-      std::make_unique<ImOsm::RichDestinationCalcWidget>(_markStorage)};
+  std::unique_ptr<ImOsm::Rich::DestinationCalcWidget> _destinationCalcWidget{
+      std::make_unique<ImOsm::Rich::DestinationCalcWidget>(_markStorage)};
 
   const std::string _iniFileNameMain{"imosm_demo.ini"};
   const std::string _iniFileNameMark{"imosm_mark.ini"};
